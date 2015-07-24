d069ede6f905c1f3661347ac94b5112728728472
walsh9 <walsh9@gmail.com>
2015-07-24 00:35:05 -0400

    Update Indentation

--- cachematrix.R
+++ cachematrix.R
@@ -26,8 +26,8 @@ cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     inv <- x$getinverse()
     if(!is.null(inv)) {
-      message("getting cached data")
-      return(inv)
+        message("getting cached data")
+        return(inv)
     }
     data <- x$get()
     inv <- solve(data, ...)
